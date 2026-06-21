57f7b01c1c646847a5a90fdadb12a5a37c1ea50d
Vusivious <118552158+programmingwithvusi@users.noreply.github.com>
2026-06-21 19:39:36 +0200

    modified .toml file update publish directory to just dist

--- frontend/src/services/vehicles.ts
+++ frontend/src/services/vehicles.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 import type { Vehicle } from "@shared/models";
 
 export async function getVehicles(): Promise<Vehicle[]> {
-    const res = await axios.get("http://localhost:3000/api/vehicles");
+    const res = await axios.get("http://localhost:3000/api/vehicles | ${env.VITE_API_BASE}");
     return res.data;
 }

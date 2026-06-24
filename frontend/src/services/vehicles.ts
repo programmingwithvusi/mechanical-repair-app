6fb9e235f3aa2003107b05476f26f36bb179d375
Vusivious <118552158+programmingwithvusi@users.noreply.github.com>
2026-06-25 00:36:46 +0200

    reinitial the commit

--- frontend/src/services/vehicles.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import type { Vehicle } from "@shared/models";
-
-export async function getVehicles(): Promise<Vehicle[]> {
-    const res = await axios.get("http://localhost:3000/api/vehicles");
-    return res.data;
-}

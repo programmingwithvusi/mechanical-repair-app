4dac36a853ea81652cc4d1b249d7e90c77dcc699
Vusivious <118552158+programmingwithvusi@users.noreply.github.com>
2026-06-28 17:04:30 +0200

    add test to nav to repair shop page and add repair job

--- frontend/tests/app.spec.ts
+++ frontend/tests/app.spec.ts
@@ -14,8 +14,12 @@ test('user can add a repair job', async ({ page }) => {
     await page.goto('http://localhost:3002/repair-jobs');
     await page.waitForLoadState('networkidle')
     //await page.waitForLoadState('networkidle');  // wait for API calls to complete
-    await page.waitForSelector('text=+ Add Repair Job', { state: 'visible' }); // wait for button text to be visible
-    await page.getByRole('button', { name: '+ Add Repair Job' }).click();
+    await page.waitForSelector('text=+ Add Repair Job', { state: 'visible', timeout: 60000 });
+    // wait for button text to be visible
+    //await page.getByRole('button', { name: '+ Add Repair Job' }).click();
+    // Using regex to ignore exact spacing
+    await page.locator('text=/.*Add Repair Job.*/').click();
+
 });
 /*
 test('repair job tab loads', async ({ page }) => {

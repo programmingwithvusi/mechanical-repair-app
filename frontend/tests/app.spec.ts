476b6aad27019dc8b9b88293bd5179bc305fcd06
Vusivious <118552158+programmingwithvusi@users.noreply.github.com>
2026-06-28 15:34:17 +0200

    add test to nav to repair shop page and add repair job

--- frontend/tests/app.spec.ts
+++ frontend/tests/app.spec.ts
@@ -8,6 +8,13 @@ test('correct ttile tab loads', async ({ page }) => {
     await expect(page).toHaveTitle('Repair Shop');
 
 });
+
+test('user can add a repair job', async ({ page }) => {
+    await page.goto('/repair-jobs');
+    await page.waitForLoadState('networkidle');  // wait for API calls to complete
+    await page.waitForSelector('text=+ Add Repair Job', { state: 'visible' }); // wait for button text to be visible
+    await page.click('text=+ Add Repair Job');
+});
 /*
 test('repair job tab loads', async ({ page }) => {
     await page.goto('/repair-jobs');
